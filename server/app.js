523431224ddcef8d4f827ef1c9095a93cdc52e16
sgdg <seungdeng17@gmail.com>
2020-03-15 22:35:00 +0900

    add. json 받아 올 express 로컬 서버 구축

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const productJSON = require('../src/json/product.json');
+const walletJSON = require('../src/json/wallet.json');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/vm/products', (req, res) => {
+  res.send(productJSON);
+});
+app.get('/vm/wallet', (req, res) => {
+  res.send(walletJSON);
+});
+
+app.listen(8080);
+
+module.exports = app;
