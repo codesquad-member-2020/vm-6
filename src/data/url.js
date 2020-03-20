76f5c1223a706364136bedae5063e501991c600b
Lee SeEun <sally4405@naver.com>
2020-03-20 15:48:38 +0900

    refector. 코드 리뷰 내용을 바탕으로 1차 리팩토링
    - cors 는 프로그램 소스코드에 영향을 주므로 dependency에 포함시킴
    - url.js 에서 _DATA -> _API
    - change model의 cash unit 내림차순 배열을 변수화 함
    - cash unit count 유효성 체크 위치 변경
    - observable의 INIT 타입 이름 'INIT'-> 'INIT_VENDING_MACHINE'
    - template 파일 디렉토리 위치 변경 util -> data

--- src/data/url.js
+++ src/data/url.js
@@ -1,7 +1,7 @@
 const URL = {
     DEV: {
-        PRODUCT_DATA: 'http://localhost:8080/vm/product',
-        WALLET_DATA: 'http://localhost:8080/vm/wallet'
+        PRODUCT_API: 'http://localhost:8080/vm/product',
+        WALLET_API: 'http://localhost:8080/vm/wallet'
     }
 }
 
